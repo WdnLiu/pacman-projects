fd48562d401d3fce5d57d6c3ff4e3e149df2c604
AdriaLeon <adria.leon01@estudiant.upf.edu>
2024-11-17 18:38:39 +0100

    ex3 Done

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -29,38 +29,43 @@ def question2():
     answer_noise = 0.01 # We make actions more predictable while rewarding long term solutions (answer_discount left as it was)
     return answer_discount, answer_noise
 
+# answer_discount (γ): Represents how much the agent values future rewards compared to immediate rewards.
+# answer_noise: Represents the probability that the agent’s action results in an unintended outcome. (if risk is up then it takes safer paths)
+# answer_living_reward: Value that the agent gets regardless where it is.
+
 def question3a():
-    answer_discount = 0.3
-    answer_noise = 0
-    answer_living_reward = 0
+    answer_discount = 0.5  # Focus on mid/short-term rewards (+1)
+    answer_noise = 0 # No fear to the risk since it doesn't contemplate an unexpected move
+    answer_living_reward = -1 # Big penalty on living so it prefers the first positive terminal state
     return answer_discount, answer_noise, answer_living_reward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answer_discount = 0.3
-    answer_noise = 0.2
-    answer_living_reward = 0.1
+    answer_discount = 0.5 # Focus on mid/short-term rewards (+1)
+    answer_noise = 0.2 # Avoids the risk
+    answer_living_reward = -1 # Big penalty on living so it prefers the first positive terminal state
     return answer_discount, answer_noise, answer_living_reward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answer_discount = 0.9
-    answer_noise = 0
-    answer_living_reward = 0
+    answer_discount = 0.9 # Focus on long-term rewards
+    answer_noise = 0 # No fear to the risk since it doesn't contemplate an unexpected move
+    answer_living_reward = 0 # No hurry for finishing earlier so it look for the higher terminal state
     return answer_discount, answer_noise, answer_living_reward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answer_discount = 0.9
-    answer_noise = 0.2
-    answer_living_reward = 0
+    answer_discount = 0.9  # Long-term rewards
+    answer_noise = 0.2    # Some uncertainty so it avoids the cliff
+    answer_living_reward = -0.1  # Slight penalty for each step
     return answer_discount, answer_noise, answer_living_reward
+
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answer_discount = 1
-    answer_noise = 0
-    answer_living_reward = 1
+    answer_discount = 0.1  # Avoid focusing on future rewards
+    answer_noise = 0       # Complete control
+    answer_living_reward = 1  # Strong incentive to keep moving
     return answer_discount, answer_noise, answer_living_reward
     # If not possible, return 'NOT POSSIBLE'
 
